baec63ffd97d95dc958b1c689450310631778f79
Muhammed Anshid <anshianshid58@gmail.com>
2026-05-22 12:17:15 +0530

    name changed

--- wash_pilot/asgi.py
+++ wash_pilot/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for carwash_project project.
+ASGI config for wash_pilot project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carwash_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wash_pilot.settings')
 
 application = get_asgi_application()
--- wash_pilot/wsgi.py
+++ wash_pilot/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for carwash_project project.
+WSGI config for wash_pilot project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carwash_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wash_pilot.settings')
 
 application = get_wsgi_application()
