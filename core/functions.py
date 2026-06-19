965fb05584f98bbaba5d4b558a1271dd24604903
Muhammed Anshid <anshianshid58@gmail.com>
2026-06-19 18:00:32 +0530

    Fix duplicate key auto_id bug by using Max aggregate in get_auto_id

--- core/functions.py
+++ core/functions.py
@@ -2,15 +2,20 @@ from .models import *
 from django.utils import timezone
 
 def get_auto_id(model):
-    auto_id = 1
+    from django.db.models import Max
     try:
-        latest_auto_id =  model.objects.all().order_by("-date_added")[:1]
-        if latest_auto_id:
-            for auto in latest_auto_id:
-                auto_id = auto.auto_id + 1
-    except:
-        pass
-    return auto_id
+        max_val = model.objects.all().aggregate(Max('auto_id'))['auto_id__max']
+        return (max_val or 0) + 1
+    except Exception:
+        auto_id = 1
+        try:
+            latest_auto_id =  model.objects.all().order_by("-date_added")[:1]
+            if latest_auto_id:
+                for auto in latest_auto_id:
+                    auto_id = auto.auto_id + 1
+        except Exception:
+            pass
+        return auto_id
 
 def log_activity(created_by, description, created_date=None):
     
